5fb9d46158fdfc66f61b17893323c82dd95b333a
u.irene <janelake2017@gmail.com>
2020-04-24 18:08:00 +0300

    Create main.cs
    main

---
+++ main 2.cs
@@ -0,0 +1,11 @@
+using System;
+
+class MainClass {
+  public static void Main (string[] args) {
+    Console.WriteLine("Hello World");
+    int a = 4, b = 3;
+    int c = a + b;
+    Console.WriteLine(c);
+    
+  }
+}
